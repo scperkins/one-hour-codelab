829d90bbabca4e18bfb0eb0bf7b6e6edfea04a49
Sean Perkins <perkinsean@gmail.com>
2015-04-09 22:02:40 -0400

    basics working

--- web/1-blankbadge/piratebadge.dart
+++ web/1-blankbadge/piratebadge.dart
@@ -2,6 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-void main() {
-  // Your app starts here.
+import 'dart:html';
+
+void  main() {
+  querySelector('#inputName').onInput.listen(updateBadge);
+}
+
+void updateBadge(Event e) {
+  querySelector('#badgeName').text = (e.target as InputElement).value;
 }
